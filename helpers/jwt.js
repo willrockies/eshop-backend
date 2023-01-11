941f3a813b7310364050669fa23122bbb605ecba
willrockies <wlsn.ads@gmail.com>
2023-01-11 11:50:44 -0300

    disable backend Authentication for apis for frontend

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,32 +1,31 @@
 // protecting the api and authentication JWT Middleware
 //const expressJwt = require('express-jwt');
-const expressJwt = require('express-jwt').expressjwt;
+const expressJwt = require("express-jwt").expressjwt;
 
 function authJwt() {
-    const secret = process.env.secret
-    const api = process.env.API_URL
-    return expressJwt({
-        secret,
-        algorithms: ['HS256'],
-    }).unless({
-        path: [
-            { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
-            { url: /\/public\/uploads(.*)/, methods: ['GET', 'OPTIONS'] },
-            { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
-            `${api}/users/login`,
-            `${api}/users/register`,
-
-        ]
-    })
+  const secret = process.env.secret;
+  const api = process.env.API_URL;
+  return expressJwt({
+    secret,
+    algorithms: ["HS256"],
+  }).unless({
+    path: [
+      // { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
+      // { url: /\/public\/uploads(.*)/, methods: ['GET', 'OPTIONS'] },
+      // { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
+      // `${api}/users/login`,
+      // `${api}/users/register`,
+      { url: /(.*)/ },
+    ],
+  });
 }
 
 async function isRevoked(req, payload, done) {
-    if(!payload.isAdmin) {
-        done(null, true)
-    }
+  if (!payload.isAdmin) {
+    done(null, true);
+  }
 
-    done();
+  done();
 }
 
-
-module.exports = authJwt
+module.exports = authJwt;
